657e3446e8e7ac490bded638638fd88dd08e379b
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-09-28 15:48:44 -0500

    navbar edits

--- react-app/src/components/UserModal/UserMenu.js
+++ react-app/src/components/UserModal/UserMenu.js
@@ -15,8 +15,8 @@ function UserMenu({ setShowUserModal, sessionUser }) {
   return (
     <div id="usermenu-overall-container">
       <div id="user-text">Hi, {sessionUser.firstName} {sessionUser.lastName}</div>
-      <NavLink id="user-your-saved-cars" to='/cars/your-saved-cars' onClick={closeModal}><i class="fa-regular fa-heart"></i> SavedCars ({sessionUser.userSavedCars.length})</NavLink>
-      <NavLink id="user-your-garage" to='/cars/your-garage' onClick={closeModal}><i class="fa-solid fa-tag"></i> Garage ({sessionUser.garageCars.length})</NavLink>
+      <NavLink id="user-your-saved-cars" to='/cars/your-saved-cars' onClick={closeModal}><i class="fa-regular fa-heart"></i> Saved Cars ({sessionUser.userSavedCars.length})</NavLink>
+      <NavLink id="user-your-garage" to='/cars/your-garage' onClick={closeModal}><i class="fa-solid fa-tag"></i> Your Garage ({sessionUser.garageCars.length})</NavLink>
       <div id="user-logout-button"  onClick={closeModal}>
         <LogoutButton />
       </div>
