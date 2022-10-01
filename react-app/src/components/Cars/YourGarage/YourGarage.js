85eb67701768bdee1b97512a3357459ed2ebfa83
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-09-30 20:10:50 -0500

    did some css to your-garage

--- react-app/src/components/Cars/YourGarage/YourGarage.js
+++ react-app/src/components/Cars/YourGarage/YourGarage.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { deleteCarThunk } from "../../../store/cars";
 import { getUserThunk } from "../../../store/session";
 import "./YourGarage.css";
 
@@ -8,15 +9,27 @@ const YourGarage = () => {
   const sessionUser = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showConfirmationId, setShowConfirmationId] = useState(0);
+  const [forceRender, setForceRender] = useState(false);
+
+
   console.log('sessionUser in YourGarage Component', sessionUser)
   console.log('sessionUser.garageCars', sessionUser.garageCars)
 
   useEffect(() => {
-    dispatch(getUserThunk(sessionUser.id))
-  }, [dispatch])
+    const timer = setTimeout(() => {
+      dispatch(getUserThunk(sessionUser.id));
+    }, 100)
+  }, [dispatch, forceRender])
 
   if (!sessionUser) return <>Session User Not Loaded</>
 
+  const handleDelete = carId => {
+    dispatch(deleteCarThunk(carId))
+    setShowConfirmation(false)
+    setForceRender(!forceRender)
+  }
 
   return (
     <div id="your-garage-overall-container">
@@ -33,6 +46,20 @@ const YourGarage = () => {
                       {car?.images?.length === 0 && <img id="your-garage-car-image" src='https://www.willow-car-sales.co.uk/wp-content/uploads/2019/11/placeholder-image-1.jpg' alt={`car ${car.id}`}  />}
                       {car?.images?.length > 0 && <img id="your-garage-car-image" src={car?.images[0]?.imageUrl} alt={`car ${car.id}`}  />}
                     </div>
+                    <span id='delete-car-icon' onClick={() => {
+                        setShowConfirmation(!showConfirmation)
+                        setShowConfirmationId(car.id)
+                      }}><i class="fa-solid fa-trash-can"></i></span>
+                      {showConfirmation && showConfirmationId === car.id && (
+                        <div id='your-garage-confirmation-container'>
+                          <div id='your-garage-confirmation-text'>Are you sure you want to delete this Car?</div>
+                          <div id='your-garage-confirmation-text-small'>This action cannot be undone.</div>
+                          <div id='your-garage-confirmation-options-container'>
+                            <div id='your-garage-confirmation-yes' onClick={() => handleDelete(car.id)}>Yes</div>
+                            <div id='your-garage-confirmation-no' onClick={() => setShowConfirmation(false)}>No</div>
+                          </div>
+                        </div>
+                      )}
                     <div id="your-garage-car-details-wrapper">
                       <div id="your-garage-conditions-make-model-container">
                         <div id="your-garage-new-label">{car.new ? 'New': 'Used'}</div>
@@ -50,7 +77,11 @@ const YourGarage = () => {
             })}
           </div>
           <div id="your-garage-right-side-container">
-            <div id="your-garage-add-car">right side</div>
+            <div id="your-garage-right-side-wrapper">
+              <div id="your-garage-add-car-title">Add a car</div>
+              <div id="your-garage-add-car-text">Need to add a car to your garage? Click "Add car details" to start. We'll ask you to enter some details about your vehicle and then upload up to 12 images.</div>
+              <NavLink id="your-garage-add-car-button" to='/cars/new'>Add car details</NavLink>
+            </div>
           </div>
         </div>
       </div>
